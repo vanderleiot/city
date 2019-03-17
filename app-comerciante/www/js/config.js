ea8da7cbec60b22e583f6bcd96598ccb2741a480
vanderleiot <central@hostbdc.com.br>
2019-03-17 10:24:22 -0300

    okes
    edreee

---
+++ app-comerciante/www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={					
+	'ApiUrl':"http://www.guiarango.com.br/merchantapp/api",
+	'DialogDefaultTitle':"Guia Rango - Entrega",
+	'APIHasKey':"df284e28ad6eb6e254fa12b787831806",
+	'debug': false
+};
